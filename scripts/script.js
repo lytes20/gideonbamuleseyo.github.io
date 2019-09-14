128e83d1bc3d60c738ed55cffe54dc81b3a34768
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2019-09-14 20:45:59 +0300

    Add change theme functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,30 @@
 const toggleThemeButton = document.getElementById("toggle-theme");
+let isDark = false;
 
-toggleThemeButton.addEventListener("click", function() {
+toggleThemeButton.addEventListener("click", changeToDarkTheme);
+// colors
+// #333
 
-});
+/**
+ * Handles changing the themes
+ * @returns {void}
+ */
+function changeToDarkTheme() {
+  const containerDiv = document.getElementsByClassName("container");
+  const cardDiv = document.getElementById("card");
+  if (!isDark) {
+    document.body.style.backgroundColor = "#121212";
+    //   setting styles
+    containerDiv[0].style.color = "#90caf9";
+    Object.assign(cardDiv.style, { border: "1px solid #333" });
+    toggleThemeButton.classList.add("button-dark-theme");
+
+    isDark = true;
+  } else {
+    document.body.style.backgroundColor = "white";
+    containerDiv[0].style.color = "black";
+    Object.assign(cardDiv.style, { border: "1px solid #333" });
+    toggleThemeButton.classList.remove("button-dark-theme");
+    isDark = false;
+  }
+}
