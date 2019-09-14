28bccff303f678c7d4029849b0bf2a74774094be
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2019-09-14 21:00:19 +0300

    Add Roboto google font and Style a tag

--- scripts/script.js
+++ scripts/script.js
@@ -12,19 +12,24 @@ toggleThemeButton.addEventListener("click", changeToDarkTheme);
 function changeToDarkTheme() {
   const containerDiv = document.getElementsByClassName("container");
   const cardDiv = document.getElementById("card");
+  const aTag = document.getElementsByTagName('a');
+
   if (!isDark) {
     document.body.style.backgroundColor = "#121212";
     //   setting styles
     containerDiv[0].style.color = "#90caf9";
-    Object.assign(cardDiv.style, { border: "1px solid #333" });
+    Object.assign(cardDiv.style, { border: "1px solid #333", backgroundColor:"#333" });
     toggleThemeButton.classList.add("button-dark-theme");
+    aTag[0].style.color = "white"
 
     isDark = true;
   } else {
     document.body.style.backgroundColor = "white";
     containerDiv[0].style.color = "black";
-    Object.assign(cardDiv.style, { border: "1px solid #333" });
+    Object.assign(cardDiv.style, { border: "1px solid #333", backgroundColor:"white" });
     toggleThemeButton.classList.remove("button-dark-theme");
+    aTag[0].style.color = "#f48fb1"
+
     isDark = false;
   }
 }
