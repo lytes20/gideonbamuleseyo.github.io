558e82ce7ad7277e1f6ecb6e089206e7a67c0f4d
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2020-01-02 16:07:06 +0300

    Add social media links

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,9 @@
 const toggleThemeButton = document.getElementById("toggle-theme");
-let isDark = false;
+let themePreference = localStorage.getItem("themePreference")
+
+const containerDiv = document.getElementsByClassName("container");
+const cardDiv = document.getElementById("card");
+const aTag = document.getElementsByTagName("a");
 
 toggleThemeButton.addEventListener("click", changeToDarkTheme);
 // colors
@@ -10,26 +14,32 @@ toggleThemeButton.addEventListener("click", changeToDarkTheme);
  * @returns {void}
  */
 function changeToDarkTheme() {
-  const containerDiv = document.getElementsByClassName("container");
-  const cardDiv = document.getElementById("card");
-  const aTag = document.getElementsByTagName('a');
+  let isDarkThemeSelected = JSON.parse(localStorage.getItem("isDark"))
+  console.log(isDarkThemeSelected)
+  setUp(isDarkThemeSelected);
+  localStorage.setItem("isDark", !isDarkThemeSelected);
+}
 
-  if (!isDark) {
+function setUp(selection) {
+  console.log("Setup is called with", selection);
+  if (!selection) {
     document.body.style.backgroundColor = "#121212";
     //   setting styles
     containerDiv[0].style.color = "#90caf9";
-    Object.assign(cardDiv.style, { border: "1px solid #333", backgroundColor:"#333" });
+    Object.assign(cardDiv.style, {
+      border: "1px solid #333",
+      backgroundColor: "#333"
+    });
     toggleThemeButton.classList.add("button-dark-theme");
-    aTag[0].style.color = "white"
-
-    isDark = true;
+    aTag[0].style.color = "white";
   } else {
     document.body.style.backgroundColor = "white";
     containerDiv[0].style.color = "black";
-    Object.assign(cardDiv.style, { border: "1px solid #333", backgroundColor:"white" });
+    Object.assign(cardDiv.style, {
+      border: "1px solid #333",
+      backgroundColor: "white"
+    });
     toggleThemeButton.classList.remove("button-dark-theme");
-    aTag[0].style.color = "#f48fb1"
-
-    isDark = false;
+    aTag[0].style.color = "#f48fb1";
   }
 }
