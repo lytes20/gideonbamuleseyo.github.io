12d4d69c3b785a9874d8e29ee13d2abcb149123f
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2020-10-12 16:08:34 +0300

    Add scroll down arrow

--- data/index.js
+++ data/index.js
@@ -61,25 +61,25 @@ const createLeadershipExp = (name, website, imagePath) => {
 
 var workExperience = [
   createWorkExperience(
-    "Ramani",
+    "ramani.io",
     "Andela is an engineering as a service business that helps companies build teams quickly and cost effectively so they can ship faster.",
     "https://www.ramani.io/",
     "./images/ramani_green_logo.png"
   ),
   createWorkExperience(
-    "Andela",
+    "andela.com",
     "Andela is an engineering as a service business that helps companies build teams quickly and cost effectively so they can ship faster.",
     "https://andela.com/",
     "./images/andela_logo.png"
   ),
   createWorkExperience(
-    "iBuild Global Inc",
+    "ibuild.global",
     "iBuild is software as a service that is closing the gap in affordable housing production",
     "https://www.ibuild.global/",
     "./images/ibuild_logo.png"
   ),
   createWorkExperience(
-    "Kola studios",
+    "kolastudios.com",
     "Kola studios is a mobile game and application development company.",
     "https://kolastudios.com/",
     "./images/kola_logo.png"
